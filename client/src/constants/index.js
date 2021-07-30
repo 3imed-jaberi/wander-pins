01875455eb8cb7b0c8a9d80c04109eed92f4b6d7
3imed-jaberi <imed_jebari@hotmail.fr>
2021-07-30 03:06:02 +0200

    add constants 📌 ..

---
+++ client/src/constants/index.js
@@ -0,0 +1,12 @@
+export const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+}
+
+export const DEFAULT_OFFSET = {
+  left: -19,
+  top: -37
+}
+
+export const DEFAULT_PIN_ICON_SIZE = 40
